262acf78a515cda04f48f6473c5fc96970f5d160
zhaza <Khail.ballin12@yahoo.com>
2022-02-01 17:51:27 -0800

    container

--- app.js
+++ app.js
@@ -1,33 +1,16 @@
-const container = document.querySelector("main");
+const container = document.querySelector(".container");
 
 for (let i = 0; i <= 16*16-1; i++) {
     const square = document.createElement("div");
-    square.textContent = i;
     square.classList.add("square");
     container.appendChild(square);
-}
+};
 
-const divs = document.querySelectorAll("div");
-const classes = divs.classList;
+const divs = document.querySelectorAll(".square");
 
 divs.forEach((div) => {
     div.addEventListener("mouseover", event => {
-        div.style.backgroundColor = "rgba(0, 0, 0, .5)";
-    })
-    div.addEventListener("mouseout", event => {
-        div.style.backgroundColor = "white";
-    })
-    div.addEventListener("click", function() {
-        const result = classes.toggle("c");
-        if (result) {
-        div.textContent = "X";
-        } else {
-        div.textContent = "O";
-        }
+        div.style.backgroundColor = "rgba(0, 0, 0)";
     });
 });
-  
-
-  
-
 
